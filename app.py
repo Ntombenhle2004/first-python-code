c3e368a1debddaf7cda24a51f88bb921ede63ac5
ntorh <ngcobontombenhle434@gmail.com>
2026-02-25 10:46:06 +0200

    learning python

---
+++ app.py
@@ -0,0 +1,15 @@
+print ("Hello World😊")
+
+print ("*" * 10)
+
+x = 1 
+y = 2
+unit_price = 3
+
+# variebles
+
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = "Python programming"
+print (students_count)
